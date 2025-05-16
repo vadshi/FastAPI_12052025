efe2e2c9d989e230cf1f7f6f1ca0fac46c1c28c4
Vadim Shikhovtsov <vadimx30@gmail.com>
2025-05-16 10:35:21 +0300

    Add config file for db

--- lib_SQLAlchemy/fastapi_books/database.py
+++ lib_SQLAlchemy/fastapi_books/database.py
@@ -1,16 +1,15 @@
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import sessionmaker, declarative_base, Session
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession,async_sessionmaker
-    
-DATABASE_URL_ASYNC = "sqlite+aiosqlite:///./a_books.db"
-DATABASE_URL = "sqlite:///./books.db"
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession,async_sessionmaker, AsyncAttrs
+from config import settings    
 
-# engine = create_engine(DATABASE_URL)
-async_engine = create_async_engine(DATABASE_URL_ASYNC, echo=False)
 
-# session_sync = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+async_engine = create_async_engine(settings.get_db_url(), echo=False)
+
 session_async = async_sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
-Base = declarative_base()
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True # класс абстрактный
+
 
 async def get_db():
     db = session_async()
@@ -20,8 +19,4 @@ async def get_db():
         await db.close()
 
 
-# async def get_db_async():
-#     async with session_async() as session:
-#         query = select(Book).filter(**kwargs)
-#         await session.execute(query)
 
