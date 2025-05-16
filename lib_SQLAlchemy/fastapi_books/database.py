0d6909e59d92eb105cb428b3709bda40d894be1b
Vadim Shikhovtsov <vadimx30@gmail.com>
2025-05-16 14:48:34 +0300

    Update justfile

--- lib_SQLAlchemy/fastapi_books/database.py
+++ lib_SQLAlchemy/fastapi_books/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession,async_sessi
 from config import settings    
 
 
-async_engine = create_async_engine(settings.get_db_url(), echo=True)
+async_engine = create_async_engine(settings.get_db_url(), echo=False)
 
 session_async = async_sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
 
